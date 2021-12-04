5585c5cd78ed001f1757bdc57bb7fe04cb76ad3c
bgduarte <57222160+bgduarte@users.noreply.github.com>
2021-12-04 13:54:12 -0300

    Modifica o grafo para ser representado por uma lista de adjacencias e atualiza varias funçoes

--- modules/grafo.py
+++ modules/grafo.py
@@ -1,46 +1,54 @@
 from .aresta import Aresta
 
 class Grafo:
-    def __init__(self, vertices, arestas, peso_mapper):
+    def __init__(self, rotulos, listaDeAdj, peso_mapper, listaDePesos):
         # Lista de rotulos
-        self.__vertices = vertices
+        self.__rotulos = rotulos
         # Lista de objetos arestas
-        self.__arestas = arestas
+        self.__listaDeAdj = listaDeAdj # para cada vertice há uma lista de vizinhos
         # mapeia o peso de cada aresta
-        peso_mapper(self.__arestas)
+        peso_mapper(self.__listaDeAdj)
+        self.__listaDePesos = listaDePesos # igual a lista adjacencia, mas com os pesos das arestas
 
     # Retorna quantidade de vertices
     def qtdVertices(self):
-        return len(self.__vertices)
+        return len(self.__rotulos)
     
     # Retorna a quantidade de arestas
     def qtdArestas(self):
-        return len(self.__arestas)
+        quantidade = 0
+        for i in range(self.qtdVertices()):
+            for adj in self.__listaDeAdj[i]:
+                if(adj >= i):
+                    quantidade+=1
+        return quantidade
 
     # Retorna o grau do vertice v
-    def grau(self, v):
-        grau = 0
-        for aresta in self.__arestas:
-            if(aresta.u == v or aresta.v == v):
-                grau+=1
-        return grau
+    def grau(self, v): # TODO:
+        pass
     
     # Retorna o rotulo do vertice v
     def rotulo(self, v):
-        return self.__vertices[v]
+        return self.__rotulos[v]
     
     # Retorna os vizinhos do vertice v
     def vizinhos(self, v):
-        pass
+        return self.__listaDeAdj[v]
     
     # Retorna True se os dois vertices estiverem ligados por uma aresta
     def haAresta(self, u, v):
-        pass
+        for vizinho in self.__listaDeAdj[u]:
+            if(vizinho == v):
+                return True
+        return False
 
     # se {u, v} pertence à E, retorna o peso da aresta {u,v}; 
     # se não existir, retorna um valor  infinito positvo(maior ponto flutuante)
     def peso(self, u, v):
-        pass
+        if(self.haAresta(u,v)):
+            for i in len(self.__listaDeAdj[u]):
+                if(self.__listaDeAdj[u][i] == v):
+                    return self.__listaDePesos[u][i]
 
     # carregar um grafo a partir de um arquivo no formato especificado
     def ler(self, arquivo):
